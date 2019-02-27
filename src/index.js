e4acdc97733124d2492bb576d5f198a3579de95d
Pulkit Singh Baliyan <duke79@users.noreply.github.com>
2019-02-27 13:53:05 +0530

    axios + mobx

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { observer } from "mobx-react";
 import { observable } from "mobx";
 import store from "./store";
 
-const App = class App extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -17,13 +17,15 @@ const App = class App extends React.Component {
 
     return (
       <div className="App">
-        <h1>Hello Hippies {store.count}</h1>
+        <h1>
+          Hello Hippies {store.count} {store.rount}
+        </h1>
         <SimpleMediaCard />
         <button onClick={() => store.increment()}>Increment</button>
       </div>
     );
   }
-};
+}
 const AppO = observer(App);
 
 const rootElement = document.getElementById("root");
--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { extendObservable, observable, action } from "mobx";
+import axios from "axios";
 
 class Store {
   increment = action("increment", function() {
@@ -14,9 +15,23 @@ class Store {
       count: 0
     });
 
+    this.rount = 0;
+
     setInterval(() => {
       this.count += 1;
+      // this.rount += 1;
     }, 1000);
+
+    let tmdb_api_home = "https://api.themoviedb.org/3";
+    let tmdb_api_key = "e30bdefcb66c118c558f0793cf22e8e2";
+    axios
+      .get(`${tmdb_api_home}/movie/top_rated?api_key=${tmdb_api_key}&page=3`)
+      .then(res => {
+        console.log(res);
+        let first_title = res.data.results[0].title;
+        // alert();
+        this.rount = first_title;
+      });
   }
 }
 
